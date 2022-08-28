44a07134f6387715951610ea5d0f0b52ed5c0dfe
anirudh1444 <111711844+anirudh1444@users.noreply.github.com>
2022-08-28 10:55:12 -0700

    Add files via upload

--- Sudoku_Solver7.java
+++ Sudoku_Solver7.java
@@ -5,51 +5,51 @@ import java.util.Scanner;
 
 public class Sudoku_Solver7 {
 
-    public static int size;
+    public static int size; //columns & rows
 
-    public static int root;
+    public static int root; //total boxes
 
-    public static List<List<Integer>> possibilities = new ArrayList<>();
+    public static List<List<Integer>> possibilities = new ArrayList<>(); //every possible digit for each box (will be the main object modified throughout the code)
 
-    public static List<Integer> changes = new ArrayList<>();
+    public static List<Integer> changes = new ArrayList<>(); //holds indices of boxes whose possibilities were changed, but not checked
 
-    public static long count = 0;
+    public static long count = 0; //total number of valid solutions
 
     public static void main (String [] args) {
         Scanner console = new Scanner(System.in);
         System.out.println("Enter the size of the sudoku (number of rows/columns): ");
         size = console.nextInt();
-        root = (int)Math.sqrt(size);
+        root = (int)Math.sqrt(size); //sets up sudoku size
 
-        int [][] sudoku = new int [size][size];
+        int [][] sudoku = new int [size][size]; //all the guaranteed/correct numbers in the solved sudoku
         List<Integer> start = new ArrayList<>();
         for (int i = 1; i <= size; i++) {
             start.add(i);
-        }
+        } //adds 1 to "size" into a list which represents all the possible digits for a box
+
         System.out.println("Enter the sudoku (place a \"0\" for empty boxes). Start from the top left and progress to the right:");
         for (int i = 0; i < Math.pow(size, 2); i++) {
             int row = i / size;
             int column = i % size;
-            int digit = console.nextInt();
+            int digit = console.nextInt(); //intakes a number from the user
             if (digit == 0) {
-                possibilities.add(start);
+                possibilities.add(start); //"0" represents an unknown value for a box; results in that box having every digit as a possibility
             } else if (digit > size) {
-                System.out.println("Invalid number. Input a different integer.");
+                System.out.println("Invalid number. Input a different integer."); //number outside of the size
                 i--;
             }
             else {
                 List<Integer> thing = new ArrayList<>();
                 thing.add(digit);
-                possibilities.add(thing);
-                sudoku[row][column] = digit;
+                possibilities.add(thing); //sets the given digit as the ONLY possibility
+                sudoku[row][column] = digit; //sets this box as the given value in the final sudoku
             }
         }
-        //System.out.println("info received");
 
         if (!isValid(sudoku)) {
             System.out.println("Invalid sudoku.");
             System.exit(0);
-        }
+        } //shuts down program if the given sudoku is invalid
 
         for (int i = 0; i < Math.pow(size, 2); i++) {
             if (possibilities.get(i).size() == 1) {
@@ -81,16 +81,34 @@ public class Sudoku_Solver7 {
     }
 
     public static boolean isValid (int [][] sudoku) {
+        /*
+        The isValid method is used to identify if the sudoku given is ever invalid.
+        The sudoku is considered invalid if any number is repeated in a row,
+        column, or sub-square.
+
+        @param sudoku This is the sudoku containing the finalized digits
+        @return boolean Returns false if the sudoku is invalid and true otherwise
+         */
+
+        //keeps count of how many times each digit ranging from 0 to size appears
         int [] values = new int [size + 1];
+
+        //checks digits within the row
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size;j++) {
+            for (int j = 0; j < size; j++) {
+                //increments the digit in the values array
                 values[sudoku[i][j]]++;
+
+                //returns false whenever a digit aside from '0' appears multiple times
                 if (values[sudoku[i][j]] > 1 && sudoku[i][j] != 0) {
                     return false;
                 }
             }
+            //resets array
             Arrays.fill(values, 0);
         }
+
+        //similar to the previous for loop, but checks digits in each column
         for (int i = 0; i < sudoku.length; i++) {
             for (int j = 0; j < sudoku.length;j++) {
                 values[sudoku[j][i]]++;
@@ -100,6 +118,8 @@ public class Sudoku_Solver7 {
             }
             Arrays.fill(values, 0);
         }
+
+        //similar to previous 2 loops, but checks digits within each sub-square
         for (int i = 0; i < root; i++) {
             for (int j = 0; j < root; j++) {
                 for (int k = i * root; k < (i + 1) * root; k++) {
@@ -113,26 +133,53 @@ public class Sudoku_Solver7 {
                 Arrays.fill(values, 0);
             }
         }
+
+        //returns true when no digit has been repeated
         return true;
     }
 
-    public static boolean isUnfinished(int [][] sudoku) { // checks if sudoku is completed
+    public static boolean isUnfinished(int [][] sudoku) {
+        /*
+        This method checks to see if the sudoku is completed by searching for a '0' in the array,
+        indicating an unknown value
+
+        @param sudoku This is the sudoku containing the finalized digits
+        @return boolean Returns true if the sudoku is unfinished
+         */
+
         for (int i = 0; i < sudoku.length; i++) {
             for (int j = 0; j < sudoku[0].length; j++) {
+
+                //returns true when the sudoku is missing a digit
                 if (sudoku[i][j] == 0) {
                     return true;
                 }
             }
         }
+
+        //returns false when box contains a digit ranging from 1 to size
         return false;
     }
 
     public static void removeColumn(int column, int value, List<List<Integer>> possibilities) {
+        /*
+        The removeColumn method removes a certain value as a possibility from an entire column
+
+        @param column This is the column number from which a digit should be removed as a possible option
+        @param value This is the digit which should be removed as a possibility
+        @param possibilities This is the list of lists containing all the possible digits for each box
+         */
+
+        //loop travels from one row to another within the same column
         for (int k = 0; k < size; k++) {
             if (possibilities.get(size * k + column).contains(value)) {
+
+                //creates a new list identical to the possibilities list for that box and removes the specified value
                 ArrayList<Integer> extra = new ArrayList<>(possibilities.get(size * k + column));
                 extra.remove((Integer) value);
                 possibilities.set(size * k + column, extra);
+
+                //any box which has value removed as a possibility is added to the changes list
                 if (!changes.contains(size * k + column)) {
                     changes.add(size * k + column);
                 }
@@ -141,6 +188,16 @@ public class Sudoku_Solver7 {
     }
 
     public static void removeRow (int row, int value, List<List<Integer>> possibilities) {
+        /*
+        The removeRow method removes a certain value as a possibility from an entire row.
+        This functions very similarly to the removeColumn method
+
+        @param row This is the row number from which a digit should be removed as a possible option
+        @param value This is the digit which should be removed as a possibility
+        @param possibilities This is the list of lists containing all the possible digits for each box
+         */
+
+        //loop travels across the row; rest of the function matches previous one
         for (int k = 0; k < size; k++) {
             if (possibilities.get(size * row + k).contains(value)) {
                 ArrayList<Integer> extra = new ArrayList<>(possibilities.get(size * row + k));
@@ -153,7 +210,18 @@ public class Sudoku_Solver7 {
         }
     }
 
-    public static void removeBox (int row, int column, int value, List<List<Integer>> possibilities) {
+    public static void removeSubSquare(int row, int column, int value, List<List<Integer>> possibilities) {
+        /*
+        The removeSubSquare method removes a certain value as a possibility from the sub-square.
+        This functions very similarly to the removeColumn and removeRow methods
+
+        @param row This is the row number of the box which is causing the changes
+        @param column This is the column number of the box which is causing the changes
+        @param value This is the digit which should be removed as a possibility
+        @param possibilities This is the list of lists containing all the possible digits for each box
+         */
+
+        //loop goes through each box in a sub-square
         for (int k = row / root * root; k < row / root * root + root; k++) {
             for (int l = column / root * root; l < column / root * root + root; l++) {
                 if (possibilities.get(size * k + l).contains(value)) {
@@ -169,43 +237,82 @@ public class Sudoku_Solver7 {
     }
 
     public static void removeSingles(int index, int [][] sudoku, List<List<Integer>> options) {
+        /*
+        Whenever a box has only one possible option, it is guaranteed that it is the correct digit.
+        When this occurs, the removeSingles method will remove this digit as a possible option in the
+        appropriate row, column, and sub-square.
+
+        @param index This is the box number
+        @param sudoku This is the 2-D array which contains the finalized digits
+        @param options This is essentially just the possibilities list
+         */
+
+        //identify row and column given the box number
         int row = index / size;
         int column = index % size;
+
+        //calls 3 functions to remove this digit as a possibility from each the corresponding row, column, and sub-square
         if (options.get(index).size() == 1) {
             int digit = options.get(index).get(0);
             sudoku[row][column] = digit;
             removeRow(row, digit, options);
             removeColumn(column, digit, options);
-            removeBox(row, column, digit, options);
+            removeSubSquare(row, column, digit, options);
         }
     }
 
     public static void nakedDoublesRC (int index, List<List<Integer>> possibilities) {
+        /*
+        This method compares the possibilities of the box of the given index with those in the same row and column.
+        In short, a naked double is when two boxes in the same row, column, or sub-square
+        have exactly 2 possible digits - both of which are the same for both boxes. When this occurs,
+        no other box in the row, column, or sub-square may contain those 2 digits. For example,
+        if two boxes in the same row both have possible solutions of 4 and 7, then no other box in the row
+        may contain 4 or 7. Therefore, we can remove them as possibilities for the rest of the row.
+
+        This method performs the aforementioned activities ONLY for rows and columns.
+
+        @param index This is the box number
+        @param possibilities This is the compilation of possible digits
+         */
+
         int row = index / size;
         int col = index % size;
-        for (int other = row * size; other < (row + 1 ) * size; other++) {
+
+        //traverses through each box in the same row as the parameter
+        for (int other = row * size; other < (row + 1) * size; other++) {
+
+            //checks to see if the two boxes have the same 2 possible digits
             if (possibilities.get(index).equals(possibilities.get(other)) && possibilities.get(index).size() == 2 && index != other) {
+
+                //checks to see if the changes list already contains "other"
                 boolean isPresent = false;
                 if (changes.contains(other)) {
                     isPresent = true;
                 }
+
+                //obtain the two digits
                 int digit1 = possibilities.get(index).get(0);
                 int digit2 = possibilities.get(index).get(1);
 
+                //remove the two digits as possibilities from the rest of the row
                 removeRow(row, digit1, possibilities);
                 removeRow(row, digit2, possibilities);
-
                 possibilities.get(index).add(digit1);
                 possibilities.get(index).add(digit2);
-
                 possibilities.get(other).add(digit1);
                 possibilities.get(other).add(digit2);
 
+                /*The removeRow function will place "other" into the changes list despite already showing up.
+                Thus, we must remove it if it was not already in there before this function was performed.
+                 */
                 if (!isPresent) {
                     changes.remove((Integer) other);
                 }
             }
         }
+
+        //same as previous loop, but checks for naked doubles in the column
         for (int other = col; other < Math.pow(size, 2); other += size) {
             if (possibilities.get(index).equals(possibilities.get(other)) && possibilities.get(index).size() == 2 && other != index) {
                 boolean isPresent = false;
@@ -233,8 +340,15 @@ public class Sudoku_Solver7 {
     }
 
     public static void nakedDoubleBox (int box, List<List<Integer>> possibilities) {
+        /*
+        This method works very similarly to the previous one. However, this method addresses naked doubles
+        within a sub-square as opposed to rows or columns.
+         */
+
         int row = box / size;
         int col = box % size;
+
+        //loop works similarly to those in the other naked doubles function, but for sub-squares
         for (int comparison_row = row / root * root; comparison_row < row / root * root + root; comparison_row++) {
             for (int comparison_col = col / root * root; comparison_col < col / root * root + root; comparison_col++) {
                 boolean isPresent = false;
@@ -247,8 +361,8 @@ public class Sudoku_Solver7 {
                     int digit1 = possibilities.get(box).get(0);
                     int digit2 = possibilities.get(box).get(1);
 
-                    removeBox(row, col, digit1, possibilities);
-                    removeBox(row, col, digit2, possibilities);
+                    removeSubSquare(row, col, digit1, possibilities);
+                    removeSubSquare(row, col, digit2, possibilities);
 
                     possibilities.get(box).add(digit1);
                     possibilities.get(box).add(digit2);
@@ -265,6 +379,23 @@ public class Sudoku_Solver7 {
     }
 
     public static void finalResort(int [][]sudoku, List<List<Integer>> available, int current) {
+        /*
+        The finalResort method's name accurately describes its function and purpose. It highlights the last
+        strategy to solve more difficult Sudokus: backtracking. Essentially, this method tries every
+        combination of possible digits for each box. If the resultant Sudoku is valid, the program will
+        print it. However, recursion and backtracking are incredibly inefficient codes and can seriously increase
+        run time for Sudokus with multiple solutions. Therefore, in order to combat this problem, the method simply
+        guesses a single digit. Afterwards, it functions exactly like earlier - it uses the aforementioned techniques
+        to obtain more information. When the code can no longer identify more digits, it reverts back to the brute
+        force method and guesses another digit. This cycle continues until the sudoku is completed.
+
+        @param sudoku Only for this method, the sudoku parameter contains guessed or possibly incorrect values
+        @param available This is simply the possibilities object, but the code can tamper with this list, preserving
+        the original possibilities
+        @param current This is the box which the code is checking. If this box already contains a digit, the program
+        will continue. Otherwise, it will guess a digit.
+         */
+
         if (current < Math.pow(size, 2)) {
             int [][] sudoku_copy = new int [size][size];
             for (int j = 0; j < size; j++) {
@@ -328,6 +459,13 @@ public class Sudoku_Solver7 {
     }
 
     public static void printSudoku(int [][] sudoku) {
+        /*
+        This method prints out a fully completed sudoku with appropriate spacing to make
+        reading the final solution clean and straightforward.
+
+        @param sudoku: a correct solution
+         */
+
         for (int i = 0; i < sudoku.length; i++) {
             for (int j = 0; j < sudoku[0].length; j++) {
                 System.out.print(sudoku[i][j] + " ");
